bec2b88cdccfc4c89d3d1df8804d4e2f42061c26
YC <774139813@qq.com>
2020-11-05 20:17:00 +0800

    init

---
+++ pages/login/login.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<view id="user-content">
+			<input type="text" id="user-input" placeholder="请输入用户名" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	#user-content {
+		width: 536upx;
+		height: 78upx;
+		background-image: url(../../static/image/user.png);
+		background-repeat: no-repeat;
+		background-size: cover;
+		padding-left: 120upx;
+
+		input {
+			height: 100%;
+			
+		}
+	}
+</style>
