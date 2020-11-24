abbcbba24e0cb458b48bae8e1de57ffd04964372
YC <774139813@qq.com>
2020-11-24 13:59:00 +0800

    upload

---
+++ components/tabbar/tabbar.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<view>
+			<view class="u-page">
+				<!-- 所有内容的容器 -->
+			</view>
+			<!-- 与包裹页面所有内容的元素u-page同级，且在它的下方 -->
+			<u-tabbar v-model="current" :list="list" :mid-button="true"></u-tabbar>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+						iconPath: "/static/tababr/page.png",
+						selectedIconPath: "home-fill",
+						text: '资源',
+						isDot: true,
+						customIcon: false,
+					},
+					{
+						iconPath: "https://cdn.uviewui.com/uview/common/min_button.png",
+						selectedIconPath: "https://cdn.uviewui.com/uview/common/min_button_select.png",
+						text: '上传',
+						midButton: true,
+						customIcon: false,
+					},
+					{
+						iconPath: "account",
+						selectedIconPath: "account-fill",
+						text: '我的',
+						isDot: false,
+						customIcon: false,
+					},
+				],
+				current: 0
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<u-tabs :list="list" :is-scroll="false" :current="current" @change="change" bg-color="#f1f1f1"></u-tabs>
+		
+		<tabbar></tabbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+					name: '全部'
+				}, {
+					name: '视频'
+				}, {
+					name: '图片'
+				}, {
+					name: '文档'
+				}, {
+					name: '其他'
+				}],
+				current: 0
+			}
+		},
+		methods: {
+			change(index) {
+				this.current = index;
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
